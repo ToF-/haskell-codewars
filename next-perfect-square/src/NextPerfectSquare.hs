b821607a5d9d8ab7e7c197049daae8a803e59e51
Christophe Thibaut <cthibaut@octo.com>
2023-04-22 10:15:51 +0200

    fixed next-perfect-square

--- next-perfect-square/src/NextPerfectSquare.hs
+++ next-perfect-square/src/NextPerfectSquare.hs
@@ -1,7 +1,16 @@
 module NextPerfectSquare where
 
 findNextSquare :: Integer -> Integer
-findNextSquare n = 
-  let rt = sqrt n
-      ps = (round rt) == truncate rt
-   in if ps then (rt + 1) * (rt + 1) else -1
+findNextSquare n | isPerfectSquare n = nextSquare n
+                 | otherwise = -1
+
+isPerfectSquare :: Integer -> Bool
+isPerfectSquare n = fromIntegral (truncate rt) == rt
+    where
+        rt :: Double
+        rt = sqrt (fromInteger n)
+
+nextSquare :: Integer -> Integer
+nextSquare n = rt * rt
+    where
+        rt = 1 + round (sqrt (fromInteger n))
